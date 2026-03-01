73b89f9068f86039f04c218d674cf999cbcec80d
Philipp <philippsiebenforcher@gmail.com>
2026-03-01 16:57:29 +0100

    First Commit

---
+++ FluidNC/esp32/esp32s3/Console.cpp
@@ -0,0 +1,32 @@
+#include <sdkconfig.h>
+#include <esp_idf_version.h>
+
+#if defined(CONFIG_TINYUSB_CDC_ENABLED) && ESP_IDF_VERSION_MAJOR >= 5
+#    include "USBCDCChannel_IDF.h"
+#else
+#    include "USBCDCChannel.h"
+#endif
+
+USBCDCChannel CDCChannel(true);
+
+#include "UartChannel.h"
+#include "Settings.h"
+
+class UartConsole : public UartChannel {
+public:
+    UartConsole() : UartChannel(0, true) {}
+    void init() override {
+        auto uart0 = new Uart(0);
+        uart0->begin(BAUD_RATE, UartData::Bits8, UartStop::Bits1, UartParity::None);
+        UartChannel::init(uart0);
+
+#if (defined(CONFIG_TINYUSB_CDC_ENABLED) && ESP_IDF_VERSION_MAJOR >= 5) || defined(ARDUINO_USB_MODE)
+        auto cdc_enable = new EnumSetting("USB CDC Enable", WEBSET, WG, NULL, "USBCDC/Enable", true, &onoffOptions);
+        if (cdc_enable->get()) {
+            CDCChannel.init();
+        }
+#endif
+    }
+};
+UartConsole Uart0;
+Channel&    Console = Uart0;
